884ac09cf6743b0acc9c66cff3726743c226e1e8
Takeshi Hayakawa <chou_22@icloud.com>
2019-08-11 17:04:10 +0900

    nuxt.js 導入

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <form action="">
+      
+    </form>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/signin.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <div class="inner_block">
+      <div class="mail_text"><input type="email" name="email" v-model="email"></div>
+      <div class="pass_text"><input type="password" name="password" v-model="password"></div>
+      <button v-on:click="regist">登録</button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  data(){
+    return{
+      email: '',
+      password: ''
+    }
+  },
+  methods:{
+    regist(){
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container{
+  text-align: center;
+}
+input,
+button{
+  border: none;
+  outline: none;
+  appearance: none;
+
+  background: rgba(148, 122, 109, 0.2);
+}
+</style>
